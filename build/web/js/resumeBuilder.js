67e9dcfd06f579c7e6433b0ce4280a7062ce0b9b
Lexa Grasz <agrasz@elon.edu>
2015-09-27 19:32:27 -0400

    udacity javascripts

---
+++ build/web/js/resumeBuilder.js
@@ -0,0 +1,64 @@
+/*
+This is empty on purpose! Your code to build the resume will go here.
+ */
+
+//var name = "Lexa Grasz";
+//
+//var formattedName = HTMLheaderName.replace("%data%", name);
+//
+//var role = "Web Developer";
+//
+//var formattedRole = HTMLheaderRole.replace("%data%", role);
+//
+//$("#header").prepend(formattedRole);
+//$("#header").prepend(formattedName);
+//
+//var skills = ["awesomeness", "programming", "JS"];
+//
+//$("#main").append(skills);
+//
+//var bio = {
+//    //"name" : "Lexa",
+//    "role" : "Web Designer",
+//    "contacts" : {
+//        "mobile" : "781-635-2681",
+//        "email" : "agrasz@elon.edu",
+//        "github" : "agrasz",
+//        "twitter" : "@janedoe",
+//        "location" : "Elon"
+//    },
+//    "welcomeMessage" : "hey hey hey",
+//    "skills" : skills,
+//    
+//    "bioPic": "images/fry.jpg"
+//};
+//
+//
+//$("#main").append(bio.name);
+//
+//var work = {};
+//work.position = "Course Developer";
+//work.employer = "Udacity";
+//work.years = 1;
+//
+//var education = {};
+//education["name"] = "Elon University";
+//education["years"] = "2014-2018";
+//education["city"] = "Elon, NC, US";
+//
+//$("#main").append(work["[position"]);
+//$("#main").append(education.name);
+//
+
+
+var education = {
+    "schools" : [
+        {
+            "name" = "Elon University",
+            "years" = "2014-2018",
+            "city" = "Elon, NC, US", 
+            "degree"= "Bachelor of Science",
+            "majors" = ["CS", "Mathematics"]
+        }
+    ]
+}
